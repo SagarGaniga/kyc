e0580004b1f89eeb39661abb9e0fab75c29b72a0
Sagar Ganiga <2015sagar.ganiga@ves.ac.in>
2017-06-23 21:23:31 -0700

    Add Cards

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello World Again</h1>
+    @include('inc.title')
+    <div class="mt-2 col-md-12">
+    </div>
+
+    @include('inc.cards')
 @endsection
